45b63c00b2f44a10b2bb707da626d417b6e4357a
squatharp <168323686+squatharp@users.noreply.github.com>
2026-05-10 16:55:32 -0600

    cors updt 2.0

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB()
 const app = express()
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://tu-frontend.vercel.app'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
